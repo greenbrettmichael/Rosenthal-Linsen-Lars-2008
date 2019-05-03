0cc6bb76a6b7cb18785a25013e4beeba55275b1c
green_machine <personal@brettmichaelgreen.com>
2019-05-03 15:28:00 -0400

    read in PLY file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,71 @@
-int main()
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+#include <vector>
+#include "third-party/tinyply/source/tinyply.h"
+
+int pointStride = 6;
+
+std::vector<float> readPLY(std::filesystem::path const& PLYpath)
 {
-    return 0;
+  
+  if (!std::filesystem::exists(PLYpath))
+  {
+    throw std::invalid_argument(PLYpath.string() + " does not exist");
+  }
+  std::ifstream ss(PLYpath, std::ios::binary);
+  if (ss.fail())
+  {
+    throw std::runtime_error(PLYpath.string() + " failed to open");
+  }
+  tinyply::PlyFile file;
+  file.parse_header(ss);
+  auto vertices = file.request_properties_from_element("vertex", { "x", "y", "z", "nx", "ny", "nz" });
+  file.read(ss);
+  if (!vertices)
+  {
+    throw std::invalid_argument(PLYpath.string() + " is missing elements required");
+  }
+  const size_t numVerticesBytes = vertices->buffer.size_bytes();
+  std::vector<float> PLYdata(pointStride*vertices->count);
+  std::memcpy(PLYdata.data(), vertices->buffer.get(), numVerticesBytes);
+  return PLYdata;
+}
+
+void displayHelp()
+{
+  std::cout << "Usage: Rosenthal-Linsen-Lars-2008 \"PLY PATH\"" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    displayHelp();
+    return 1;
+  }
+  std::vector<float> PLYdata;
+  try
+  {
+    PLYdata = readPLY(argv[1]);
+  }
+  catch (std::invalid_argument const& e)
+  {
+    std::cout << "BAD PLY FILE" << std::endl;
+    std::cerr << e.what() << std::endl;
+    return 2;
+  }
+  catch (std::runtime_error const& e)
+  {
+    std::cout << "ENCOUNTERED ERROR READING PLY FILE " << std::endl;
+    std::cerr << e.what() << std::endl;
+    return 3;
+  }
+  catch (std::exception const& e)
+  {
+    std::cout << "ENCOUNTERED ERROR READING PLY FILE " << std::endl;
+    std::cerr << e.what() << std::endl;
+    return 4;
+  }
+  return 0;
 }
